d03177e7bf984ebf9e94fee979fc319275c2caa9
timalexis.koerner <timalexis.koerner@stud.uni-goettingen.de>
2021-05-25 17:53:22 +0200

    reworked qcd, qd_data and data of circuits are now kept in sync

--- schroedinger/circuit/quditcircuitdata.py
+++ schroedinger/circuit/quditcircuitdata.py
@@ -28,56 +28,210 @@
 #
 ###############################################################################
 
-"""A wrapper class for the purposes of validating modifications to
-QuditCircuit.qddata while maintaining the interface of a python list."""
+"""A wrapper class for the purposes of validating modifications to both QuditCircuit.data
+and QuditCircuit.qd_data while maintaining the interface of a python list.
+Keeps the data and qd_data synchronized,
+as each item of data is a subset of the item of qd_data at the same index."""
 
 from qiskit.circuit.quantumcircuitdata import QuantumCircuitData
+from qiskit.circuit.instruction import Instruction
 from qiskit.circuit.exceptions import CircuitError
 
 from .quditinstruction import QuditInstruction
 
 
 class QuditCircuitData(QuantumCircuitData):
-    """A wrapper class for the purposes of validating modifications to
-    QuditCircuit.qddata while maintaining the interface of a python list."""
+    """A wrapper class for the purposes of validating modifications to QuditCircuit.data
+    and QuditCircuit.qd_data while maintaining the interface of a python list."""
 
-    def __getitem__(self, i):
-        return self._circuit._qddata[i]
+    def qd_get(self, key):
+        """Returns data tuple including qudit context at index key or
+         a list of tuples with slice as key."""
+        return self._circuit._qd_data[key]
+
+    def __getitem__(self, key):
+        """
+        Supports imaginary numbers (e.g. 0j) for indexing into qd_data tuples.
+
+        Arg:
+            key (int or complex or slice or list): Index of the data tuple to be retrieved.
+        Returns:
+            Data tuple / list of data tuples with qudit context if key is a purely imaginary
+            integer / slice with purely imaginary integers.
+            Data tuple / list of data tuples without qudit context if key is a real
+            integer / slice with real integers.
+
+        Raises:
+            TypeError: If the `key` is complex but not a purely imaginary integer.
+            TypeError: If the `key` is a slice with different index types (not regarding None).
+        """
+        if isinstance(key, complex):
+            if key.real != 0 or int(key.imag) != key.imag:
+                raise TypeError("Complex keys must be purely imaginary integers.")
+
+            return self.qd_get(int(key.imag))
+
+        if isinstance(key, slice):
+            slice_types = set(type(i) for i in (key.start, key.stop, key.step) if i is not None)
+            if len(slice_types) > 1:
+                raise TypeError("All slice indices must either have the same type or be None.")
+
+            if any(type(idx) == complex for idx in (key.start, key.stop, key.step)):
+
+                if any(idx.real != 0 or int(idx.imag) != idx.imag
+                       for idx in (key.start, key.stop, key.step) if idx is not None):
+                    raise TypeError("Complex slice indices must be purely imaginary integers.")
+
+                return self.qd_get(slice(
+                    *(int(idx.imag) if idx is not None else None
+                      for idx in (key.start, key.stop, key.step))
+                ))
+
+        return super().__getitem__(key)
 
     def __setitem__(self, key, value):
-        instruction, qdargs, qargs, cargs = value
+        """
+        Supports imaginary numbers (e.g. 0j) to get qd_data tuples.
 
-        if not isinstance(instruction, QuditInstruction) and \
-                hasattr(instruction, "to_qdinstruction"):
-            instruction = instruction.to_qdinstruction()
+        Arg:
+            key int: Index of the data tuple to set.
+            value tuple: Data tuple containing instruction, qdargs (optional), qargs, cargs.
+
+        Raises:
+            CircuitError: If the instruction is not a valid Instruction.
+            CircuitError: If the instruction is not a QuditInstruction but `value` includes qdargs.
+            CircuitError: If `value` cannot be interpreted without broadcasting.
+        """
+        instruction, qargs, cargs = value[0], value[-2], value[-1]
+
+        if len(value) == 4:
+            qdargs = value[2]
+        else:
+            qdargs = []
+
+        expanded_qdargs = [self._circuit.qdit_argument_conversion(qdarg)
+                           for qdarg in qdargs or []]
+        expanded_qargs = [self._circuit.qbit_argument_conversion(qarg)
+                          for qarg in qargs or []]
+        expanded_cargs = [self._circuit.cbit_argument_conversion(carg)
+                          for carg in cargs or []]
+
+        if qdargs:
+            if not isinstance(instruction, QuditInstruction) and \
+                    hasattr(instruction, "to_qd_instruction"):
+                instruction = instruction.to_qd_instruction()
 
             if not isinstance(instruction, QuditInstruction):
-                raise CircuitError("Object is not an QuditInstruction.")
+                raise CircuitError(
+                    "Instruction argument is not an QuditInstruction, "
+                    "but value includes has qdargs."
+                )
 
-        expanded_qdargs = [self._circuit.qdit_argument_conversion(qdarg) for qdarg in qdargs or []]
-        expanded_qargs = [self._circuit.qbit_argument_conversion(qarg) for qarg in qargs or []]
-        expanded_cargs = [self._circuit.cbit_argument_conversion(carg) for carg in cargs or []]
+            broadcast_args = list(instruction.broadcast_arguments(
+                expanded_qdargs, expanded_qargs, expanded_cargs)
+            )
+            qdargs, qargs, cargs = broadcast_args[0]
+        else:
+            if not isinstance(instruction, Instruction) \
+                    and hasattr(instruction, 'to_instruction'):
+                instruction = instruction.to_instruction()
 
-        broadcast_args = list(instruction.broadcast_arguments(
-            expanded_qdargs, expanded_qargs, expanded_cargs)
-        )
+            if not isinstance(instruction, Instruction):
+                raise CircuitError('Instruction argument is not an Instruction.')
+
+            broadcast_args = list(instruction.broadcast_arguments(
+                expanded_qargs, expanded_cargs)
+            )
+            qargs, cargs = broadcast_args[0]
 
         if len(broadcast_args) > 1:
             raise CircuitError(
-                "QuditCircuit.qddata modification does not support argument broadcasting."
+                "Circuit data modification does not support argument broadcasting."
             )
 
-        qdargs, qargs, cargs = broadcast_args[0]
-
-        self._circuit._check_qddups(qdargs)
+        self._circuit._check_dups(qdargs)
         self._circuit._check_dups(qargs)
         self._circuit._check_qdargs(qdargs)
         self._circuit._check_qargs(qargs)
         self._circuit._check_cargs(cargs)
 
-        self._circuit._data[key] = (instruction, qdargs, qargs, cargs)
+        self._circuit._data[key] = (instruction, qargs, cargs)
+        self._circuit._qd_data[key] = (instruction, qdargs, qargs, cargs)
 
         self._circuit._update_parameter_table(instruction)
 
+    def insert(self, index, value):
+        """inserts value for data and qd_data at index"""
+        self._circuit._data.insert(index, None)
+        self._circuit._qd_data.insert(index, None)
+        self[index] = value
+
+    def __delitem__(self, i):
+        """deletes data and qd_data at index"""
+        del self._circuit._data[i]
+        del self._circuit._qd_data[i]
+
     def __cast(self, other):
-        return other._circuit._qddata if isinstance(other, QuditCircuitData) else other
+        """casts to QuditCircuitData if possible"""
+        if isinstance(other, QuditCircuitData):
+            return other._circuit._qd_data
+        if isinstance(other, QuantumCircuitData):
+            return other._circuit._data
+        return other
+
+    def __repr__(self):
+        """returns qd_data representation"""
+        return repr(self._circuit._qd_data)
+
+    def __add__(self, other):
+        """Adds qd_data to other qd_data if possible.
+        Otherwise defaults to adding data to other data."""
+        if isinstance(other, QuditCircuitData):
+            return self._circuit._qd_data + self.__cast(other)
+        return self._circuit._data + self.__cast(other)
+
+    def __radd__(self, other):
+        """Adds other qd_data to qd_data if possible.
+        Otherwise defaults to adding other data to data."""
+        if isinstance(other, QuditCircuitData):
+            return self.__cast(other) + self._circuit._qd_data
+        return self.__cast(other) + self._circuit._data
+
+    def __mul__(self, n):
+        """
+        Multiplies and returns data*n if `n` is real integer,
+        qd_data*n if `n` is a purely imaginary integer.
+
+        Raises:
+            TypeError: If the `n` is complex but not a purely imaginary integer.
+        """
+        if isinstance(n, complex):
+            if n.real != 0 or int(n.imag) != n.imag:
+                raise TypeError("Complex factors must be purely imaginary integers.")
+
+            return self._circuit._qd_data * int(n.imag)
+        return self._circuit._data * n
+
+    def __rmul__(self, n):
+        """
+        Multiplies and returns n*data if `n` is real integer,
+        n*qd_data if `n` is a purely imaginary integer.
+
+        Raises:
+            TypeError: If the `n` is complex but not a purely imaginary integer.
+        """
+        if isinstance(n, complex):
+            if n.real != 0 or int(n.imag) != n.imag:
+                raise TypeError("Complex factors must be purely imaginary integers.")
+
+            return int(n.imag) * self._circuit._qd_data
+        return n * self._circuit._data
+
+    def sort(self, *args, **kwargs):
+        """In-place stable sort of both data and qd_data. Accepts arguments of list.sort."""
+        self._circuit._data.sort(*args, **kwargs)
+        self._circuit._qd_data.sort(*args, **kwargs)
+
+    def qd_copy(self):
+        """Returns a shallow copy of qudit instruction list."""
+        return self._circuit._qd_data.copy()
